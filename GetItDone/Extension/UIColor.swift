05319eecdbfa7decd41fdccebd863e909ffa4ea2
Martin Bing <bing.martin.photography@gmail.com>
2019-01-04 15:23:01 +0100

    Add and use static colors

--- GetItDone/Extension/UIColor.swift
+++ GetItDone/Extension/UIColor.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 extension UIColor {
+    
+    static var blueZero: UIColor {
+        return UIColor.init(rgb: 0x64E4FF)
+    }
+    static var blueOne: UIColor {
+        return UIColor.init(rgb: 0x3A7BD5)
+    }
+    
     convenience init(red: Int, green: Int, blue: Int) {
         self.init(
             red: CGFloat(red) / 255.0,
@@ -16,6 +24,14 @@ extension UIColor {
             blue: CGFloat(blue) / 255.0,
             // if you don't add alpha here it will go for the convenience init, which expects Int iso CGFloat
             alpha: 1.0
+        )
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
         )
     }
 }
--- GetItDone/MBGradient.swift
+++ GetItDone/MBGradient.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class MBGradient: UIView {
     
+    // ------ Example Purpose only, unused ----------------
     var colors: [CGColor] = [
         UIColor.init(red: 100, green: 128, blue: 255).cgColor,
         UIColor.init(red: 58, green: 123, blue: 213).cgColor
     ]
+    
+    var hexColors: [CGColor] = [
+        UIColor.init(rgb: 0x64E4FF).cgColor,
+        UIColor.init(rgb: 0x3A7BD5).cgColor
+    ]
+    // -----------------------------------------------------
+    
+    var staticColors: [CGColor] = [
+        UIColor.blueZero.cgColor,
+        UIColor.blueOne.cgColor
+    ]
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.translatesAutoresizingMaskIntoConstraints = false
         if let layer = self.layer as? CAGradientLayer {
-            layer.colors = self.colors
+            layer.colors = self.staticColors
             layer.locations = [0.0, 1.2]
             
         }
